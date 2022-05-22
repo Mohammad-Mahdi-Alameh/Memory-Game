0e8c897af350445d41f067514928f0b130760df8
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-05-22 10:03:05 +0300

    Adding the audio files using javascript

---
+++ scripts/script .js
@@ -0,0 +1,19 @@
+let green_btn=document.getElementById("green");
+
+let red_btn=document.getElementById("red");
+
+let yellow_btn=document.getElementById("yellow");
+
+let blue_btn=document.getElementById("blue");
+
+let header=document.getElementById("header");
+
+let green_sound=new Audio("../assets/sounds/green.mp3");
+
+let red_sound=new Audio("../assets/sounds/red.mp3");
+
+let yellow_sound=new Audio("../assets/sounds/yellow.mp3");
+
+let blue_sound=new Audio("../assets/sounds/blue.mp3");
+let wrong_sound=new Audio("../assets/sounds/wrong.mp3");
+
