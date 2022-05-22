4e909837ce02a9c5e8c52f6644eb03eff1cb0956
Mohammad-Mahdi-Alameh <mmwalameh@gmail.com>
2022-05-22 07:56:06 +0300

    Taking references in js to all HTML elements

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+let green_btn=document.getElementById("green");
+
+let red_btn=document.getElementById("red");
+
+let yellow_btn=document.getElementById("yellow");
+
+let blue_btn=document.getElementById("blue");
+
+let header=document.getElementById("header");
